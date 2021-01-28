2bd5349ba543b92bda83f989956a42f9068bb717
FiestaLake <FiestaLake@protonmail.com>
2021-01-28 04:33:43 +0000

    modules: afk.py: Add response for replying
    Signed-off-by: FiestaLake <FiestaLake@protonmail.com>

--- tg_bot/modules/afk.py
+++ tg_bot/modules/afk.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 from telegram import Message, Update, Bot, User
 from telegram import MessageEntity
+from telegram.error import BadRequest
 from telegram.ext import Filters, MessageHandler, run_async
 
 from tg_bot import dispatcher
@@ -52,20 +53,30 @@ def reply_afk(bot: Bot, update: Update):
                 if not user_id:
                     # Should never happen, since for a user to become AFK they must have spoken. Maybe changed username?
                     return
-                chat = bot.get_chat(user_id)
+
+                try:
+                    chat = bot.get_chat(user_id)
+                except BadRequest:
+                    print("Error: Could not fetch userid {} for AFK module".
+                          format(user_id))
+                    return
                 fst_name = chat.first_name
 
             else:
                 return
 
-            if sql.is_afk(user_id):
-                valid, reason = sql.check_afk_status(user_id)
-                if valid:
-                    if not reason:
-                        res = "{} is AFK!".format(fst_name)
-                    else:
-                        res = "{} is AFK! says its because of:\n{}".format(fst_name, reason)
-                    message.reply_text(res)
+    elif message.reply_to_message:
+        user_id = message.reply_to_message.from_user.id
+        fst_name = message.reply_to_message.from_user.first_name
+
+    if sql.is_afk(user_id):
+        valid, reason = sql.check_afk_status(user_id)
+        if valid:
+             if not reason:
+                    res = "{} is AFK!".format(fst_name)
+             else:
+                    res = "{} is AFK! says its because of:\n{}".format(fst_name, reason)
+             message.reply_text(res)
 
 
 def __gdpr__(user_id):
@@ -84,8 +95,7 @@ __mod_name__ = "AFK"
 AFK_HANDLER = DisableAbleCommandHandler("afk", afk)
 AFK_REGEX_HANDLER = DisableAbleRegexHandler("(?i)brb", afk, friendly="afk")
 NO_AFK_HANDLER = MessageHandler(Filters.all & Filters.group, no_longer_afk)
-AFK_REPLY_HANDLER = MessageHandler(Filters.entity(MessageEntity.MENTION) | Filters.entity(MessageEntity.TEXT_MENTION),
-                                   reply_afk)
+AFK_REPLY_HANDLER = MessageHandler(Filters.all & Filters.group, reply_afk)
 
 dispatcher.add_handler(AFK_HANDLER, AFK_GROUP)
 dispatcher.add_handler(AFK_REGEX_HANDLER, AFK_GROUP)
