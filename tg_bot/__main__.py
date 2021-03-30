e5e85336cc132165088a4606771fb7ff0a7ae161
Sung Mingi <FiestaLake@protonmail.com>
2021-03-31 01:24:00 +0900

    __main__.py: Ignore AttributeError
    /tgbot/tg_bot/__main__.py", line 444, in process_update
        cnt = CHATS_CNT.get(update.effective_chat.id, 0)
    AttributeError: 'NoneType' object has no attribute 'id'
    
    Signed-off-by: Sung Mingi <FiestaLake@protonmail.com>

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -441,7 +441,10 @@ def process_update(self, update):
         return
 
     now = datetime.datetime.utcnow()
-    cnt = CHATS_CNT.get(update.effective_chat.id, 0)
+    try:
+        cnt = CHATS_CNT.get(update.effective_chat.id, 0)
+    except AttributeError:
+        return
 
     t = CHATS_TIME.get(update.effective_chat.id, datetime.datetime(1970, 1, 1))
     if t and now > t + datetime.timedelta(0, 1):
